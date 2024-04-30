8f4c4b3295b6211fa7b3865cb09ae131cc338810
Ye Mon Aung <yemonaung202@gmail.com>
2024-04-30 17:39:51 +0630

    WIP on the visitor pattern experimentation.
    The implementation of the pattern itself is not quite complex.
    The problem lies with the need to have the specialized types
    (BinaryExpr, Grouping, etc) to have a member of type Expr, which is
    the base type of all those specialized types. And the base type is
    supposed to define a pure virtual function which "accepts" the
    visitor. The compiler isn't quite happy about that.

--- lox/lox_ast.cpp
+++ lox/lox_ast.cpp
@@ -1,3 +1,15 @@
 #include "lox_lexer.h"
 #include "lox_ast.h"
 
+void Expr::accept(const ExprVisitor & visitor)
+{
+}
+
+void BinaryExpr::accept(const ExprVisitor & visitor)
+{
+}
+
+void Grouping::accept(const ExprVisitor & visitor)
+{
+}
+
--- lox/lox_ast.h
+++ lox/lox_ast.h
@@ -2,12 +2,16 @@
 #define LOX_AST_
 
 #include <string.h>
+#include <variant>
 #include "lox_lexer.h"
 
 using string = std::string;
 using string_view = std::string_view;
 
+struct ExprVisitor;
+
 struct Expr {
+    virtual void accept(const ExprVisitor &visitor);
 };
 
 struct BinaryExpr : Expr {
@@ -18,12 +22,16 @@ struct BinaryExpr : Expr {
     BinaryExpr(Expr left, Token optr, Expr right) :
         left(left), optr(optr), right(right) 
     {}
+
+    void accept(const ExprVisitor &visitor) override;
 };
 
 struct Grouping : Expr {
     Expr expression;
     
     Grouping(Expr expression) : expression(expression) {}
+
+    void accept(const ExprVisitor &visitor) override;
 };
 
 struct Literal : Expr {
@@ -38,6 +46,23 @@ struct Unary : Expr {
     Expr right;
 
     Unary(Token optr, Expr right) : optr(optr), right(right) {}
+};
+
+//--------------------------------------------------------------------------------
+
+struct ExprVisitor {
+    virtual ~ExprVisitor() = default;
+    virtual void visit(const BinaryExpr &expr) const = 0;
+    virtual void visit(const Grouping &group) const = 0;
+    virtual void visit(const Literal &literal) const = 0;
+    virtual void visit(const Unary &unary) const = 0;
+};
+
+struct Parser : ExprVisitor {
+    void visit(const BinaryExpr &expr) const override;
+    void visit(const Grouping &group) const override;
+    void visit(const Literal &literal) const override;
+    void visit(const Unary &unary) const override;
 };
 
 #endif
