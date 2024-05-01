5b08f34aa43d57019c0657aa46b2d45c2c2170e8
Ye Mon Aung <yemonaung202@gmail.com>
2024-05-01 19:19:52 +0630

    A couple of new mechanims for all the expression types.
    1) Factory functions which allocates each expression tree node into
    a unique_ptr<Expr>. Expressions with operands (eg., binary, unary)
    will inherantly move the source expression nodes in order to
    construct. If the source expression tree doesn't need to be deleted/
    invalidated, then there's the second accompanying mechanim.
    
    2) The CopyVisitor, which could navigate the entire tree branches
    and copy each expression nodes appropriately.
    
    The overall code looks verbose, but I'm actually quite satisfied
    with the result. And the overall logics make a lot of sense too.
    Each expression tree can and should be independent.
    If they're
    supposed to be attached to another tree, then the original tree
    should be invalidated (thus, moved).
    If an expression tree node should be re-used over multiple other
    trees, then a proper copy semantics should be applied too.

--- lox/lox_ast.cpp
+++ lox/lox_ast.cpp
@@ -1,6 +1,45 @@
 #include "lox_lexer.h"
 #include "lox_ast.h"
 
+ExprPtr 
+make_binary_expr(ExprPtr left, Token const optr, ExprPtr right)
+{
+    auto binary = std::make_unique<Expr>(BinaryExpr{
+        std::move(left),
+        optr,
+        std::move(right)
+    });
+    return binary;
+}
+
+ExprPtr 
+make_grouping(ExprPtr expr)
+{
+    auto group = std::make_unique<Expr>(Grouping{
+        std::move(expr)
+    });
+    return std::move(group);
+}
+
+ExprPtr
+make_literal(std::string const &value) 
+{
+    auto literal = std::make_unique<Expr>(Literal{ value });
+    return std::move(literal);
+}
+
+ExprPtr 
+make_unary(Token const optr, ExprPtr right)
+{
+    auto unary = std::make_unique<Expr>(Unary{
+        optr,
+        std::move(right)
+    });
+    return std::move(unary);
+}
+
+//------------------------------------------------------------------------------
+
 void test()
 {
     Expr binary = BinaryExpr{
@@ -25,32 +64,30 @@ void test()
 void test2()
 {
     // (- 123)
-    Expr left = Unary{
+    ExprPtr lit1 = make_literal("123");
+    std::visit(PrintVisitor{ true }, *lit1);
+
+    ExprPtr left = make_unary(
         Token{ TokenType::MINUS, "-", "", 1 },
-        std::make_unique<Expr>(Literal{ "123" })
-    };
-    std::visit(PrintVisitor{}, left);
-    std::cout << "\n";
+        std::move(std::visit(CopyVisitor{}, *lit1))
+    );
+    std::visit(PrintVisitor{ true }, *left);
 
     // (45.67)
-    Expr right = Grouping{
-        std::make_unique<Expr>(Literal{ "45.67" })
-    };
-    std::visit(PrintVisitor{}, right);
-    std::cout << "\n";
+    ExprPtr right = make_grouping(
+        make_literal("45.67")
+    );
+    std::visit(PrintVisitor{ true }, *right);
 
     // (* (- 123) (45.67))
-    Expr expr = BinaryExpr{
-        std::make_unique<Expr>(Unary{
-            Token{ TokenType::MINUS, "-", "", 1 }, 
-            std::make_unique<Expr>(Literal{ "123" })
-        }),
+    ExprPtr expr = make_binary_expr(
+        std::move(std::visit(CopyVisitor{}, *left)),
         Token{ TokenType::STAR, "*", "", 1 },
-        std::make_unique<Expr>(Grouping{
-            std::make_unique<Expr>(Literal{ "45.67" })
-        })
-    };
-    std::visit(PrintVisitor{}, expr);
-    std::cout << "\n";
+        std::move(std::visit(CopyVisitor{}, *right))
+    );
+    std::visit(PrintVisitor{ true }, *expr);
+
+    ExprPtr expr2 = std::move(std::visit(CopyVisitor{}, *expr));
+    std::visit(PrintVisitor{ true }, *expr2);
 }
 
--- lox/lox_ast.h
+++ lox/lox_ast.h
@@ -13,15 +13,16 @@ struct Grouping;
 struct Literal;
 struct Unary;
 
-using Expr = std::variant<BinaryExpr, Grouping, Literal, Unary>;
+using Expr     = std::variant<BinaryExpr, Grouping, Literal, Unary>;
+using ExprPtr  = std::unique_ptr<Expr>;
 
 struct BinaryExpr {
-    std::unique_ptr<Expr> left;
+    ExprPtr left;
     Token optr;
-    std::unique_ptr<Expr> right;
+    ExprPtr right;
 
     BinaryExpr() : optr(Token{}) {}
-    BinaryExpr(std::unique_ptr<Expr> left, Token optr, std::unique_ptr<Expr> right) : 
+    BinaryExpr(ExprPtr left, Token optr, ExprPtr right) : 
         left(std::move(left)),
         optr(optr),
         right(std::move(right))
@@ -29,10 +30,10 @@ struct BinaryExpr {
 };
 
 struct Grouping {
-    std::unique_ptr<Expr> expr;
+    ExprPtr expr;
     
     Grouping() {}
-    Grouping(std::unique_ptr<Expr> expr) : expr(std::move(expr)) {}
+    Grouping(ExprPtr expr) : expr(std::move(expr)) {}
 };
 
 struct Literal {
@@ -44,16 +45,25 @@ struct Literal {
 
 struct Unary {
     Token optr;
-    std::unique_ptr<Expr> right;
+    ExprPtr right;
 
     Unary() : optr(Token{}) {}
-    Unary(Token optr, std::unique_ptr<Expr> right) : 
+    Unary(Token optr) : optr(optr) {}
+    Unary(Token optr, ExprPtr right) : 
         optr(optr),
         right(std::move(right))
     {}
 };
 
-//--------------------------------------------------------------------------------
+ExprPtr make_binary_expr(ExprPtr left, Token const optr, ExprPtr right);
+
+ExprPtr make_grouping(ExprPtr expr);
+
+ExprPtr make_literal(std::string const &value);
+
+ExprPtr make_unary(Token const optr, ExprPtr right);
+
+//------------------------------------------------------------------------------
 
 struct ParseVisitor {
     void operator()(BinaryExpr const &binary) const {
@@ -69,31 +79,83 @@ struct ParseVisitor {
     }
 };
 
+struct CopyVisitor {
+    ExprPtr operator()(BinaryExpr const &binary) const {
+        auto new_binary = make_binary_expr(
+            std::move(std::visit(CopyVisitor{}, *(binary.left))),
+            binary.optr,
+            std::move(std::visit(CopyVisitor{}, *(binary.right)))
+        );
+        return new_binary;
+    }
+
+    ExprPtr operator()(Grouping const &group) const {
+        auto new_group = make_grouping(
+            std::move(std::visit(CopyVisitor{}, *(group.expr)))
+        );
+        return new_group;
+    }
+
+    ExprPtr operator()(Literal const &literal) const { 
+        auto new_literal = make_literal(literal.value);
+        return new_literal;
+    }
+
+    ExprPtr operator()(Unary const &unary) const {
+        auto new_unary = make_unary(
+            unary.optr,
+            std::move(std::visit(CopyVisitor{}, *(unary.right)))
+        );
+        return std::move(new_unary);
+    }
+};
+
 struct PrintVisitor {
+    bool line_break;
+
+    PrintVisitor() : line_break(false) {}
+    PrintVisitor(bool line_break) : line_break(line_break) {}
+
     void operator()(BinaryExpr const &binary) const {
         std::cout << "(" << binary.optr.lexeme << " ";
         std::visit(PrintVisitor{}, *(binary.left));
         std::cout << " ";
         std::visit(PrintVisitor{}, *(binary.right));
         std::cout << ")";
+        if (line_break) {
+            std::cout << '\n';
+        }
     }
 
     void operator()(Grouping const &group) const {
         std::cout << "(";
         std::visit(PrintVisitor{}, *(group.expr));
         std::cout << ")";
+        if (line_break) {
+            std::cout << '\n';
+        }
     }
 
     void operator()(Literal const &literal) const {
         std::cout << literal.value;
+        if (line_break) {
+            std::cout << '\n';
+        }
     }
 
     void operator()(Unary const &unary) const {
         std::cout << "(" << unary.optr.lexeme << " ";
-        std::visit(PrintVisitor{}, *(unary.right));
+        if (unary.right) {
+            std::visit(PrintVisitor{}, *(unary.right));
+        }
         std::cout << ")";
+        if (line_break) {
+            std::cout << '\n';
+        }
     }
 };
+
+//------------------------------------------------------------------------------
 
 void test();
 void test2();
